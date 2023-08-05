c6e1dd6ef5ece10b37c5d5be8bee49b4132f2fd9
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-08-05 22:52:20 +0400

    featured-movies added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div className="App">Hello</div>;
-}
+import React from "react";
+import HomeContainer from "../containers/home";
+const HomePage = () => {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
+};
+
+export default HomePage;
